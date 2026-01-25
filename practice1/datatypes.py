bfb897513e57dafc7e63cf4e90ee7c7715639860
Sandugash <wonychan32@gmail.com>
2026-01-25 22:26:19 +0500

    seventh  commit

---
+++ practice1/datatypes.py
@@ -0,0 +1,21 @@
+#exercise 1:
+x = range(6)
+print(type(x))
+
+#exercise 2:
+y="hi"
+print(type(x))
+
+#exercise 3:
+z=10.00
+print(type(x))
+
+#exercise 4:
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+#exercise 5:
+x = True
+print(type(x))
+
+
