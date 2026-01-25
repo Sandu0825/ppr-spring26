430cfca832579a0951ee351f6b0662ffb08d27e5
Sandugash <wonychan32@gmail.com>
2026-01-25 21:39:56 +0500

    first  commit

--- practice1/intro.py
+++ practice1/intro.py
@@ -1,1 +1,16 @@
+#exercise 1:
 print("Hello")
+
+#exercise 2:
+if 5 > 2:
+    print("YES")
+
+#exercise 3:
+    # python comment
+    
+#exercise 4:
+    """ This is a comment written in more than just one line """
+    
+#exercise 5:
+if 2<1:
+    print("Yes")
