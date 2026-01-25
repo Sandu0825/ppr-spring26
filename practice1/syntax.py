157d9ec0b5edcc960855ffcf11d8b8892b02e276
Sandugash <wonychan32@gmail.com>
2026-01-25 20:04:01 +0500

    third commit

--- practice1/syntax.py
+++ practice1/syntax.py
@@ -1,25 +1,23 @@
 #example 1:
-
 #This is a comment
-#example 1:
-print("Hello, World!")
 
 #example 2:
+print("Hello, World!")
+
+#example 3:
 if 5 > 2:
     print("Five is greater than two!")
 
-#example 3:
+#example 4:
 x = 5
 y = "Hello, World!"
 
-#example 3:
+#example 5:
 print("Hello"); print("How are you?"); print("Bye bye!")
 
-#example 4:
+#example 6:
 print("Hello World!")
 print("Have a good day.")
 print("Learning Python is fun!")
 
-#example 5:
-print("Python is fun!")
 
