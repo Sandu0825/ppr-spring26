788fabba942a81c7c0cdfebbf690121aad3e27c3
Sandugash <wonychan32@gmail.com>
2026-01-25 21:59:50 +0500

    fifth  commit

---
+++ practice1/comments.py
@@ -0,0 +1,16 @@
+#exercise 1:
+#This is a comment
+print("Hello, Everyone!")
+
+#exercise 2:
+"""This is a comment written in more than just one line"""
+print(6*7)
+
+#exercise 3:
+#print("This is a comment")
+
+#exercise 4:
+""" This is also comment """
+
+#exercise 5:
+#hiii
