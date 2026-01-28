4a3bf28e5006b3a22b464af66a89d869a1942d16
Sandugash <wonychan32@gmail.com>
2026-01-28 22:12:14 +0500

    Add Practice1 Python basics examples

---
+++ Practice_1/comments3.py
@@ -0,0 +1,6 @@
+age = 20
+# Check if the person is an adult
+if age >= 18:
+    print("Adult")  # if age >= 18
+else:
+    print("Minor")  # if age < 18
---
+++ Practice_1/comments4.py
@@ -0,0 +1,4 @@
+# Check if number is positive
+number = 10
+if number > 0:
+    print("Positive")  # only prints if number > 0
