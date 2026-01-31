2d06baf9f0ba1a6df6f33fd3896e8449dfaaf0f1
Sandugash <wonychan32@gmail.com>
2026-02-01 00:27:53 +0500

    practice

---
+++ Practice2/BooleansAsComparison/ex4.py
@@ -0,0 +1,5 @@
+name1 = "Alice"
+name2 = "Bob"
+
+print(name1 == name2)  # False, строки разные
+print(name1 != name2)  # True, строки разные
---
+++ Practice2/BooleansAsComparison/ex5.py
@@ -0,0 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [1, 2, 3]
+list3 = [3, 2, 1]
+
+print(list1 == list2) 
+print(list1 != list3)  
