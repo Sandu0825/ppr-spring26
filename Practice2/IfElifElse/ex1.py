eec7ff86da7d7d202cc4b82262c0de855a75e55a
Sandugash <wonychan32@gmail.com>
2026-02-02 22:12:19 +0500

    practice

---
+++ Practice2/IfElifElse/ex1.py
@@ -0,0 +1,8 @@
+x = 10
+
+if x > 0:
+    print("Positive number")
+elif x < 0:
+    print("Negative number")
+else:
+    print("Zero")
---
+++ Practice2/IfElifElse/ex4.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+
+if number > 10:
+    print("Number is greater than 10")
+elif number == 10:
+    print("Number is equal to 10")
+else:
+    print("Number is less than 10")
