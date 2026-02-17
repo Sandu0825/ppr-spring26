70e8cc72facc07e94c8765baa1eb348908126e6a
Sandugash <wonychan32@gmail.com>
2026-02-17 17:16:48 +0500

    practice

---
+++ Practice3/classes/class_definition.py
@@ -0,0 +1,19 @@
+#exercise 1:
+class MyClass:
+  x = 5
+  
+#exercise 2:
+p1 = MyClass()
+print(p1.x)
+
+#exercise 3:
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
